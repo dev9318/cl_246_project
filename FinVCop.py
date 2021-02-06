74dba81bc98b40d6b2ed8d6b3530a3774b445457
dev9318 <dev.moxaj@gmail.com>
2021-02-07 01:15:23 +0530

    added chnaged area

--- FinVCop.py
+++ FinVCop.py
@@ -10,15 +10,30 @@ x = []
 w, b, L = 4.0*pow(10,-3), 1.0*pow(10,-3), 17.0*pow(10,-3)
 k = 230
 h = 100
-m = pow(h*(2*w+2*b)/(k*w*b),0.5)
-M = del_T*pow(h*(2*w+2*b)*(k*w*b),0.5)
-q = M*(np.sinh(m*L)+(h/(m*k))*np.cosh(m*L))/(np.cosh(m*L)+(h/(m*k))*np.sinh(m*L))
+# m = pow(h*(2*w+2*b)/(k*w*b),0.5)
+# M = del_T*pow(h*(2*w+2*b)*(k*w*b),0.5)
+# q = M*(np.sinh(m*L)+(h/(m*k))*np.cosh(m*L))/(np.cosh(m*L)+(h/(m*k))*np.sinh(m*L))
 
-for i in range(100):
-    copn.append((i+1)*q)
+W = [i*pow(10,-3) for i in range(1,11)]
+
+for w, i in zip(W,[i for i in range(10)]):
+    m = pow(h*(2*w+2*b)/(k*w*b),0.5)
+    M = del_T*pow(h*(2*w+2*b)*(k*w*b),0.5)
+    q = M*(np.sinh(m*L)+(h/(m*k))*np.cosh(m*L))/(np.cosh(m*L)+(h/(m*k))*np.sinh(m*L))
+    copn.append((const2+q*10)/const1)
     x.append(i+1)
 
 
 plt.plot(copn,x)
 plt.show()
+
+
+
+# for i in range(100):
+#     copn.append((const2+q*i)/const1)
+#     x.append(i+1)
+
+
+# plt.plot(copn,x)
+# plt.show()
 
