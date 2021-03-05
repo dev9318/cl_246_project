26f363c1ad301418863a6de94e75b41bc67b01dc
dev9318 <dev.moxaj@gmail.com>
2021-03-05 23:19:20 +0530

    Update time_re.m

--- time_re.m
+++ time_re.m
@@ -8,9 +8,10 @@ T_h = 40 + 273;
 R = density*L/A;
 K = k*A/L;
 Z = alpha^2/(density*k);
-I = alpha * T_c/R;
+I = alpha * T_c/R; %I for max power (Q_c)
 m = 1;
 C_p = 4.2e3;
 T = 30+273;
-n=10;
-time = m*C_p*(T-T_c)/n*(alpha*T_c.*I - 1/2*I.^2.*R - K*(T_c-T_h));
+n=128;
+%time (in min)
+time = m*C_p*(T-T_c)/(n*(alpha*T_c.*I - 1/2*I.^2.*R - K*(T_c-T_h)))/60
