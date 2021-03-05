0fb77a07c99aeda2868e783c70415017dc53e0e5
dev9318 <dev.moxaj@gmail.com>
2021-03-05 23:04:48 +0530

    Update time_re.m

---
+++ time_re.m
@@ -0,0 +1,15 @@
+alpha = 400*10^(-6);
+density = 10^(-5);
+L = 2*10^(-3);
+A = 4*10^(-6);
+k = 2*1.52*10^(-2);
+T_c = 15 + 273;
+T_h = 40 + 273;
+R = density*L/A;
+K = k*A/L;
+Z = alpha^2/(density*k);
+I = alpha * T_c/R;
+m = 1;
+C_p = 4.2e3;
+T = 30+273;
+time = m*C_p*(T-T_c)/n*(alpha*T_c.*I - 1/2*I.^2.*R - K*(T_c-T_h));
